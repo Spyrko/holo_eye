a1f301880ba04c2840d85ae4c11099c969ed69b0
CSL-Hololense <csl.hololens@gmail.com>
2018-05-18 11:50:05 +0200

    init

---
+++ Assets/HoloToolkitExtensions/InitialPlaceByTap.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Unity.SpatialMapping;
+
+namespace LocalJoost.HoloToolkitExtensions
+{
+    public class InitialPlaceByTap : MonoBehaviour, IInputClickHandler
+    {
+        protected AudioSource Sound;
+        protected MoveByGaze GazeMover;
+
+        protected void Start()
+        {
+            Sound = GetComponent<AudioSource>();
+            GazeMover = GetComponent<MoveByGaze>();
+
+            InputManager.Instance.PushFallbackInputHandler(gameObject);
+        }
+
+        public virtual void OnInputClicked(InputClickedEventData eventData)
+        {
+            if (!GazeMover.IsActive)
+            {
+                return;
+            }
+
+            if (Sound != null)
+            {
+                Sound.Play();
+            }
+
+            GazeMover.IsActive = false;
+            SpatialMappingManager.Instance.DrawVisualMeshes = false;
+        }
+    }
+}
